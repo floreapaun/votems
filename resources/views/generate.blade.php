cddd2d241a40ab9939da63875c2e6d1141119880
serj99 <go_with_the_flow99@yahoo.com>
2019-03-22 19:09:59 +0200

    Added Bootstrap grid and percents graph.

--- resources/views/generate.blade.php
+++ resources/views/generate.blade.php
@@ -2,8 +2,85 @@
 
 @section('content')
 <link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/css"/>
+<style>
+span.badge.badge-primary, button.badge.badge-success 
+{
+    font-size: 110%;
+}
+</style>
 
 
+  <div class='row'>
+    <div class="col-sm-7 text-center">
+      <div class='col-sm-12'>
+        <div class="form-inline">
+          <span class="badge badge-primary">Varsta medie a votantilor partidului</span>
+          <select class="form-control" id='party' name='party'>
+              @foreach($partyname_arr as $p)
+                  <option value='{{ $p->party_name }}'>{{ $p->party_name }}</option>
+              @endforeach
+          </select> 
+          <button class="badge badge-success" id='BttnGenAvgAge' onclick="getValue(this.id)">Genereaza</button>
+        </div>
+      </div>
+      <div class='col-sm-12'>
+        <p id='AvgAge'colspan='2'></p>
+      </div>
+      <div class='col-sm-12'>
+        <div class="form-inline">
+          <span class="badge badge-primary">Numarul de voturi provenite din cel mai sarac judet din tara</span>
+          <button class="badge badge-success" id='BttnGenAvgAge' onclick="getValue(this.id)">Genereaza</button>
+        </div>
+      </div>
+      <div class='col-sm-12'>
+        <p id='AvgAge'colspan='2'></p>
+      </div>
+      <div class='col-sm-12'>
+        <div class="form-inline">
+          <span class="badge badge-primary">Topul partidelor din regiunea</span>
+          <select class="form-control" id='region'>
+            <option value='Banat'>Banat</option>
+            <option value='Crisana'>Crisana</option>
+            <option value='Dobrogea'>Dobrogea</option>
+            <option value='Maramures'>Maramures</option>
+            <option value='Moldova'>Moldova</option>
+            <option value='Muntenia'>Muntenia</option>
+            <option value='Oltenia'>Oltenia</option>
+            <option value='Transilvania'>Transilvania</option>
+          </select> 
+          <button class="badge badge-success" id='BttnGenTopRegion'>Genereaza</button>
+        </div>
+      </div>
+      <div class='col-sm-12'>
+        <p id='AvgAge'colspan='2'></p>
+      </div>
+      <div class='col-sm-12'>
+        <div class="form-inline">
+          <span class="badge badge-primary">Topul partidelor votate de tineri (18-30 ani)</span>
+          <button class="badge badge-success" id='BttnGenAvgAge' onclick="getValue(this.id)">Genereaza</button>
+        </div>
+      </div>
+      <div class='col-sm-12'>
+        <p id='AvgAge'colspan='2'></p>
+      </div>
+      <div class='col-sm-12'>
+        <div class="form-inline">
+          <span class="badge badge-primary">Topul partidelor votate de cei cu studii superioare</span>
+          <button class="badge badge-success" id='BttnGenAvgAge' onclick="getValue(this.id)">Genereaza</button>
+        </div>
+      </div>
+      <div class='col-sm-12'>
+        <p id='AvgAge'colspan='2'></p>
+      </div>
+    </div>
+    <div id="img_div" class="col-sm-3 text-center">
+      {{--
+      <img src="images/pielabelsex5.jpg" alt="">
+      --}}
+    </div>
+</div>
+
+{{--
     <table id="genpolls_tbl">
       <tr>
         <td>Varsta medie a votantilor partidului
@@ -14,7 +91,6 @@
             @endforeach
         </select>  
 
-        {{-- 
         <select id='party' name='party'>
             @php 
               $query = "SELECT nume_partid FROM partid";
@@ -27,7 +103,6 @@
               }
             @endphp 
           </select>
-         --}}
 
         </td>
         <td>
@@ -102,7 +177,7 @@
         <td id='RegWin' colspan='2'></td>
       </tr>
     </table>  
-
+--}}
 
 </div>
 
